362e91f14b0d8a06ed8724d6234cb29ba8337ca2
Ben Magro <bmagro@gmail.com>
2014-06-01 01:03:54 +1000

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+Add-SGminer-X11Mod-Miner-To-BAMT
+========================
+
+This is script will install an optimised x11 algorithm miner within BAMT 1.6.0
+
+#Minable Coins
+
+- Darkcoin (DRK)
+- X11Coin (XC)
+- Logicoin (LGC)
+- Hirocoin (HIC)
+- MUNITI (MUN)
+- Sync (SYNC)
+
+#Instructions
+
+```bash
+git clone https://github.com/benmagro/Add-SGminer-X11Mod-Miner-To-BAMT.git
+cd Add-SGminer-X11Mod-Miner-To-BAMT/
+./x11.sh
+```
+
+Then review your bamt.conf and you sgminer-x11mod.conf
+
+##Donate
+
+BTC: 1GeA7ZeKV5yH68EkMQCLb2ygm9K4qotT2x
---
+++ x11.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+mine stop
+sleep 5
+cp x11.patch /tmp/
+cp x13.patch /tmp/
+cd /opt/miners/
+git clone https://github.com/lasybear/sph-sgminer_x11mod.git sgminer-x11mod
+cd /opt/miners/sgminer-x11mod
+cp /opt/miners/sgminer-4.1.0-sph/ADL_SDK/* /opt/miners/sgminer-x11mod/ADL_SDK/
+make clean
+sleep 5
+chmod +x autogen.sh
+./autogen.sh
+sleep 2
+CFLAGS="-O2 -Wall -march=native -I /opt/AMDAPP/include/" LDFLAGS="-L/opt/AMDAPP/lib/x86" ./configure --enable-opencl
+sleep 5
+make install
+sleep 5
+clear
+cp example.conf /etc/bamt/sgminer-x11mod.conf
+cp example.conf /etc/bamt/sgminer-x13mod.conf
+cd /etc/bamt/
+patch /etc/bamt/bamt.conf <<.
+116a117,118
+>   cgminer_opts: --api-listen --config /etc/bamt/sgminer-x11mod.conf
+>   cgminer_opts: --api-listen --config /etc/bamt/sgminer-x13mod.conf
+124a127
+>   # Sgminer X11 Mod "DRK" and X13 Mod "BOST"
+129a133
+>   miner-sgminer-x11mod: 1
+.
+patch /opt/bamt/common.pl <<.
+1477a1478,1480
+>       } elsif (\${\$conf}{'settings'}{'miner-sgminer-x11mod'}) {
+>         \$cmd = "cd /opt/miners/sgminer-x11mod/;/usr/bin/screen -d -m -S sgminer-x11 /opt/miners/sgminer-x11mod/sgminer \$args";
+>         \$miner = "sgminer-x11";
+.
+cd /etc/bamt/
+mv /tmp/x11.patch /etc/bamt/
+mv /tmp/x13.patch /etc/bamt/
+cd /etc/bamt/
+patch /etc/bamt/sgminer-x11mod.conf < x11.patch
+patch /etc/bamt/sgminer-x13mod.conf < x13.patch
+rm x11.patch
+rm x13.patch
+echo 'X11Mod Miner Installed.'
+echo 'Please review your /etc/bamt/bamt.conf to enable.'
+echo 'Configure /etc/bamt/sgminer-x11mod.conf with pool'
